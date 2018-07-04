682131eda5af8e106dabc3f85beecda37771653e
vrittikwillrockdef <40247240+vrittikwillrockdef@users.noreply.github.com>
2018-07-04 10:48:31 +0530

    Error Handling basic code

---
+++ handling.py
@@ -0,0 +1,15 @@
+try:
+    a=121
+    b=456
+    f = open("ab.txt")
+    p=b/0
+
+    for line in f:
+        print(line)
+except FileNotFoundError as e:
+    print("Anything at all",e.filename)
+#except (FileNotFoundError , ZeroDivisionError):
+#    print("File not found  0xFFwegergnid0KJKH")
+#i=0/0
+#except ZeroDivisionError:
+#    print("Divided By Zero")
